6fdf0ec8d4054de6c6337fa075c564e4c52e5ef6
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-09-09 16:43:44 +0530

    deploy create note API

--- createNotes.js
+++ createNotes.js
@@ -4,16 +4,15 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
-  console.log("event.body: ", event.body);
-  // const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body);
 
   const params = {
     TableName: process.env.notesTableName,
     Item: {
-      userId: '123',
+      userId: data.userId,
       noteId: uuid.v1(),
-      content: "data.content",
-      createdAt: Date.now(),
+      content: data.content,
+      createdAt: Date.new(),
     },
   };
 
