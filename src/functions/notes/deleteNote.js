45dc845565f722dd88d9a695913772287e028da3
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-09-10 05:12:41 +0530

    add delete note by id API

--- src/functions/notes/deleteNote.js
+++ src/functions/notes/deleteNote.js
@@ -2,21 +2,15 @@ import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.notesTableName,
     Key: {
       userId: event.pathParameters.userId, // The id of the author
       noteId: event.pathParameters.id, // The id of the note from the path
     },
-    UpdateExpression: "SET content = :content",
-    ExpressionAttributeValues: {
-      ":content": data.content || null,
-    },
-    ReturnValues: "ALL_NEW",
   };
 
-  const result = await dynamoDb.update(params);
-
-  return { data: result };
+  const result = await dynamoDb.delete(params);
+  console.log({result});
+  return;
 });
