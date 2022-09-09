9b6bc39850b94f83bda8aae374ee8718db1145d5
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-09-10 04:25:42 +0530

    refactor

--- src/functions/notes/createNotes.js
+++ src/functions/notes/createNotes.js
@@ -1,9 +1,8 @@
 import * as uuid from "uuid";
-import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/dynamodb-lib";
+import handler from "../../libs/handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
   const params = {
--- src/functions/notes/getAllNotes.js
+++ src/functions/notes/getAllNotes.js
@@ -12,6 +12,6 @@ export const main = handler(async (event, context) => {
 
   const result = await dynamoDb.query(params);
   console.log(result);
-  // Return the matching list of items in response body
+
   return result.Items;
 });
